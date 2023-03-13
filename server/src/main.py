acc2cb2aab3a855a27b759d4acd5ea884390ca87
Enrique Plata <>
2023-03-13 19:59:37 +0100

    Fixing flask and running it with container

--- server/src/main.py
+++ server/src/main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+from flask import Flask
+from os import environ
 
-# Press ⇧F10 to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return "Hellow, this is an application"
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('Enrique')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    server_port = int(environ.get('PORT', '8080'))
+    app.run(debug=True, host='0.0.0.0', port=server_port)
